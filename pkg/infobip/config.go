dece8fe6764856963cd7ae3a0ed6cd0cba2ae0bc
Surahman <surahman.duang@gmail.com>
2019-12-18 09:31:24 +0700

    Add config and .env

---
+++ pkg/infobip/config.go
@@ -0,0 +1,17 @@
+package infobip
+
+import (
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+const (
+	SMS_ENDPOINT = "/sms/2/text/advanced"
+)
+
+var (
+	baseUrl   string = os.Getenv("INFOBIP_BASE_URL")
+	apiKey    string = os.Getenv("INFOBIP_API_KEY")
+	notifyUrl string = os.Getenv("INFOBIP_NOTIFY_URL")
+)
